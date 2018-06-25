258a0627b5b84d7709279548d3496a1c66f4c51a
xiaobin zhang <zhangxiaobin@live.com>
2018-06-25 16:17:54 +0800

    完成ArrayHelper.Splic功能，通过单元测试。

---
+++ ZxbLib.Collection.Test/ArrayHelperSpliceTest.cs
@@ -0,0 +1,67 @@
+using System;
+using Xunit;
+using ZxbLib.Collection.Helpers;
+
+namespace ZxbLib.Collection.Test
+{
+    public class ArrayHelperSpliceTest
+    {
+        [Fact]
+        public void InputArgumentsValidationTest()
+        {
+            int[] anull = null;
+            Assert.Throws<ArgumentNullException>(() => ArrayHelper.Splice(ref anull, 0, 0));
+
+            int[] a = { 1, 2, 3 };
+            // 0 <= sourceIndex < sourceArray.Length
+            Assert.Throws<ArgumentOutOfRangeException>(() => ArrayHelper.Splice(ref a, -1, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => ArrayHelper.Splice(ref a, a.Length, 0));
+            // length >= 0
+            Assert.Throws<ArgumentOutOfRangeException>(() => ArrayHelper.Splice(ref a, 0, -1));  
+        }
+
+        [Fact]
+        public void TrancateOneElementTest()
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6 };
+
+            int[] b = ArrayHelper.Splice(ref a, 5, 2);
+
+            Assert.Equal(new int[] { 1, 2, 3, 4, 5 }, a);
+            Assert.Equal(new int[] { 6 }, b);
+        }
+
+        [Fact]
+        public void TrancateTwoElementsTest()
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6 };
+
+            int[] b = ArrayHelper.Splice(ref a, 2, 2);
+
+            Assert.Equal(new int[] { 1, 2, 5, 6 }, a);
+            Assert.Equal(new int[] { 3, 4 }, b);
+        }
+
+        [Fact]
+        public void TrancateAllElementsTest()
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6 };
+
+            int[] b = ArrayHelper.Splice(ref a, 0, a.Length);
+
+            Assert.Equal(new int[] { }, a);
+            Assert.Equal(new int[] { 1, 2, 3, 4, 5, 6 }, b);
+        }
+
+        [Fact]
+        public void TrancateArrayAndInsertNewElementsTest()
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6 };
+
+            int[] b = ArrayHelper.Splice(ref a, 2, 2, 7, 8, 9);
+
+            Assert.Equal(new int[] { 1, 2, 7, 8, 9, 5, 6 }, a);
+            Assert.Equal(new int[] { 3, 4 }, b);
+        }
+    }
+}
---
+++ ZxbLib.Collection/Helpers/ArrayHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ZxbLib.Collection.Helpers
+{
+    public static class ArrayHelper
+    {
+        // 实现Javascript Splice函数的功能
+        public static T[] Splice<T>(ref T[] sourceArray, int sourceIndex, int length, params T[] insertedElements)
+        {
+            if (sourceArray == null)
+            {
+                throw new ArgumentNullException(nameof(sourceArray));
+            }
+            if (sourceIndex < 0 || sourceIndex >= sourceArray.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sourceIndex));
+            }
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length));
+            }
+
+            // 如果要截取的元素个数大于从sourceIndex开始到数组结束的元素个数
+            int acturalLenth = (sourceIndex + length) > sourceArray.Length ? (sourceArray.Length - sourceIndex) : length;
+
+            var deletedItems = new T[acturalLenth];
+            Array.ConstrainedCopy(sourceArray, sourceIndex, deletedItems, 0, acturalLenth);
+
+            int arrayLengthDifference = insertedElements.Length - acturalLenth;
+            int newArrayLength = sourceArray.Length + arrayLengthDifference;
+            var newArray = new T[newArrayLength];
+
+            Array.Copy(sourceArray, newArray, sourceIndex);
+            int newArrayCopyedElementsIndex = sourceIndex;
+            Array.ConstrainedCopy(insertedElements, 0, newArray, newArrayCopyedElementsIndex, insertedElements.Length);
+            newArrayCopyedElementsIndex += insertedElements.Length;
+            int remainedElementsIndex = sourceIndex + acturalLenth;
+            // 源数组末尾还有元素才进行拷贝
+            if (remainedElementsIndex < sourceArray.Length)
+            {
+                Array.ConstrainedCopy(sourceArray, remainedElementsIndex, newArray, newArrayCopyedElementsIndex, sourceArray.Length - remainedElementsIndex);
+            }
+
+            sourceArray = newArray;
+
+            return deletedItems;
+        }
+    }
+}
